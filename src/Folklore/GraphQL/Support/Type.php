8bc862d0db384a327732f36b163a27e7993841b6
AndreaMauro@Home <bibendus1983@gmail.com>
2017-08-22 16:29:46 +0200

    fix inputObjectType not included

--- src/Folklore/GraphQL/Support/Type.php
+++ src/Folklore/GraphQL/Support/Type.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Fluent;
 use Folklore\GraphQL\Support\Field;
 
 use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\InputObjectType;
 use GraphQL\Type\Definition\InterfaceType;
 
 class Type extends Fluent
